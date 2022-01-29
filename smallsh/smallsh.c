ff20816675223b47f68492a751dd3d8d39fb75d0
Jessica Dutton <jessjdutton@gmail.com>
2022-01-29 14:03:42 -0800

    first commit

---
+++ smallsh/smallsh.c
@@ -0,0 +1,19 @@
+/*
+	Author:	Jessica Dutton
+	Class:	OSU CS 344
+	Last updated: 
+	Smallsh Main
+*/
+
+#include "smallsh.h"
+
+
+/// <summary>
+/// Main function
+/// </summary>
+/// <returns>0</returns>
+int main()
+{
+
+	return 0;
+}
